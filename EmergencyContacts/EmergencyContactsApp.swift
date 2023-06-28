37093548a6317ad8c9b9a87d34cae334fa8f28c3
Vahe Abazyan <va.abazyan@gmail.com>
2023-06-29 01:30:35 +0400

    Initial Commit

---
+++ EmergencyContacts/EmergencyContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmergencyContactsApp.swift
+//  EmergencyContacts
+//
+//  Created by Vahe Abazyan on 29.06.23.
+//
+
+import SwiftUI
+
+@main
+struct EmergencyContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
